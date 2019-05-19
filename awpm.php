35d2544745f9e7cf04c4542414eb3a96054bac8e
Andrew A. Chuev <andrew.chuev@gmail.com>
2019-05-19 09:47:08 +0300

    change developer site url

--- awpm.php
+++ awpm.php
@@ -18,7 +18,7 @@
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Andrew A. Chuev
- * Author URI:        https://chuev.com
+ * Author URI:        https://chuev.online
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       awpm
